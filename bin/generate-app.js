39df341a681352e5b5c61236ac249206f09e5fdb
mansooranis <mansooranis60@gmail.com>
2023-08-15 02:06:01 -0600

    First commit

---
+++ bin/generate-app.js
@@ -0,0 +1,16 @@
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+    console.log('You have to provide a name to your app.');
+    console.log('For example :');
+    console.log('    npx create-my-boilerplate my-app');
+    process.exit(1);
+}
+
+const projectName = process.argv[2];
+const currentPath = process.cwd();
+const projectPath = path.join(currentPath, projectName);
+const git_repo = YOUR_GIT_URL;
+
